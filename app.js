adb934615733b7be93b4b74ddb7b39a5a98e529b
Ntombi20 <ntombi@projectcodex.co>
2016-02-16 15:55:22 +0200

    committing my first function for taxi kata

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 
+function keyCodeName(num) {
+    if (num === 39) {
+    	return "right";
+    }
+
+    else if (num === 37) {
+    	return "left";
+    }
+
+    else if (num === 38) {
+    	return "up";
+    }
+
+    else if (num === 40) {
+    	return "down";
+    }
+
+    else if (num === 52||32||64) {
+    	return "";
+    };
+
+};
 var body = document.querySelector("body");
 
 body.onkeydown = function(e){
