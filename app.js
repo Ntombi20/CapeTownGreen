fa83164960132af89b9015f92679857449672877
Ntombi20 <ntombi@projectcodex.co>
2016-02-18 17:22:49 +0200

    my taxi is moving

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function keyCodeName(num) {
     	return "left";
     }
 
-    else if (num === 52||32||64) {
+    else {
     	return "";
     };
 
@@ -25,30 +25,73 @@ function keyCodeName(num) {
 
  body.onkeydown = function(e){
 //     //e.keyCode - will capture the key codes
-//     //e.keyCode('click', function() {
     var keyCode = e.keyCode;
     var keyName = keyCodeName(keyCode);
-    var locationClass =createLocactionClass(taxiLocationCounter)
+    var locationClass = createLocationClass(taxiLocationCounter);
     displayMessage(locationClass);
-    //});
- };
+    
+  moveForward();
+  //   if (keyName === "forward") {
+  //       locationClass++;
+  //   }
+  //   else{
+  //       locationClass--;
+  //   }
+   }  
 
-var taxiLocationCounter = 1;
-for (var i = 0; i < taxiLocationCounter.length; i++) {
-    taxiLocationCounter[i];
 
-};
+ var taxiLocationCounter = 1;
+// //var counter = counter++;
+    for (var i = 0; i < taxiLocationCounter.length; i++) {
+        if (keyCodeName === 'forward') {
+            taxiLocationCounter = taxiLocationCounter+1;
+        }
+        else{
+            taxiLocationCounter = taxiLocationCounter-1;
+        }
+    }; 
 
 var createLocationClass = function(number) {
     if (number === 1) {
         return "slot-one-of-nine";
     }
 
+    if (number === 2) {
+        return "slot-two-of-nine";
+    }
+
+    if (number === 3) {
+        return "slot-three-of-nine";
+    }
+
+    if (number === 4) {
+        return "slot-four-of-nine";
+    }
+
     if (number === 5) {
        return "slot-five-of-nine"; 
     }
 
+    if (number === 6) {
+        return "slot-six-of-nine";
+    }
+
+    if (number === 7) {
+        return "slot-seven-of-nine";
+    }
+
+    if (number === 8) {
+        return "slot-eight-of-nine";
+    }
     if (number === 9) {
        return "slot-nine-of-nine"; 
     };
 };
+
+function moveForward(){
+    var currentLocation = createLocationClass(taxiLocationCounter);
+    taxiLocationCounter++;
+    var newLocation = createLocationClass(taxiLocationCounter);
+    moveTaxi(currentLocation, newLocation);
+};
+
